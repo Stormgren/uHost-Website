7d25dcf512d8bdd6d6074244ff55b7375362a52b
Stormgren <sbnv93@gmail.com>
2019-04-18 14:10:26 +0200

    Added function to close modal

--- shared.js
+++ shared.js
@@ -1,10 +1,20 @@
 let backdrop = document.querySelector('.backdrop');
 let modal = document.querySelector('.modal');
 let selectPlanButtons = document.querySelectorAll('.plan button');
+let modalNoButton = document.querySelector('.modal__actions button');
 
 for (let i = 0; i < selectPlanButtons.length; i++){
     selectPlanButtons[i].addEventListener('click', function(){
         modal.style.display = 'block';
         backdrop.style.display = 'block';
     })
+}
+
+backdrop.addEventListener('click', closeModal);
+
+modalNoButton.addEventListener('click', closeModal);
+
+function closeModal(){
+    modal.style.display='none';
+    backdrop.style.display = 'none';
 }
