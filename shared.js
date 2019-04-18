94d5b5aa8928a19d353ebd341c7abe2d5ba5adba
Stormgren <sbnv93@gmail.com>
2019-04-18 13:57:24 +0200

    added part of modal and button functionality by implementing eventlisteners to change display styles

--- shared.js
+++ shared.js
@@ -1,3 +1,10 @@
 let backdrop = document.querySelector('.backdrop');
+let modal = document.querySelector('.modal');
+let selectPlanButtons = document.querySelectorAll('.plan button');
 
-backdrop.style.display = 'block';
+for (let i = 0; i < selectPlanButtons.length; i++){
+    selectPlanButtons[i].addEventListener('click', function(){
+        modal.style.display = 'block';
+        backdrop.style.display = 'block';
+    })
+}
